1250eaca96c2c799646d2133786c611e5adc4c2d
tao08 <1823@holbertonschool.com>
2020-09-03 22:54:53 +0000

    len sys

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    barrera = 0
+    for i in range(1, len(sys.barrera)):
+        barrera = int(sys.barrera[i]) + barrera
+    print("{:d}".format(barrera))
