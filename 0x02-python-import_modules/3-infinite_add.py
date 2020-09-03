e15d4d051524e40549c8c300ba7deca77ab416c4
tao08 <1823@holbertonschool.com>
2020-09-03 23:03:37 +0000

    len_argv

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     barrera = 0
-    for i in range(1, len(sys.barrera)):
-        barrera = int(sys.barrera[i]) + barrera
+    for i in range(1, len(sys.argv)):
+        barrera = int(sys.argv[i]) + barrera
     print("{:d}".format(barrera))
