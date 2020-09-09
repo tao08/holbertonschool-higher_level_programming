0ad4abf99b0464876b516ddec6d930e78b72304e
tao08 <1823@holbertonschool.com>
2020-09-09 02:05:58 +0000

    modi

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    copy_my_list = my_list[:]
-    if idx < 0 or idx >= len(my_list):
-        return (copy_my_list)
-    else:
-        copy_my_list[idx] = element
-        return (copy_my_list)
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
+    my_list.reverse()
+    for n in my_list:
+        print("{:d}".format(n))
