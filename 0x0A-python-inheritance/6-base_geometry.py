b0a691a7a938a0666ab453487f78f5984822abc4
tao08 <1823@holbertonschool.com>
2021-01-20 01:16:46 +0000

    Raises Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" lookup method """
+
+
+class BaseGeometry():
+    """class"""
+    def area(self):
+        """ Raises Exception"""
+        raise Exception("area() is not implemented")
