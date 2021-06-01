108255a514c686113387024ef1e50f8ba317e177
tao08 <1823@holbertonschool.com>
2021-06-01 17:10:12 +0000

    arguments

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,37 @@
-#r/bin/python3
-# Prints the State object wih the name passed as argument from the database.
-
-import sqlalchemy
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from model_state import Base, State
+#!/usr/bin/python3
+"""
+    prints the State object with the name passed '
+    as argument from the database hbtn_0e_6_usa
+"""
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+
+    # Parameter variables
+    user = argv[1]
+    passw = argv[2]
+    database = argv[3]
+    word_to_match = argv[4]
+
+    # ᐁ Create the engine
+    engine = create_engine(
+                    'mysql+mysqldb://{}:{}@localhost/{}'
+                    .format(user, passw, database), pool_pre_ping=True
+                    )
+    # create the session instant and bind the engine
     Session = sessionmaker(bind=engine)
+    # Create the session
     session = Session()
-    item = session.query(State).filter_by(name=sys.argv[4]).first()
-    if item:
-        print("{}".format(item.id))
-    else:
-        print("Not found")
 
-    session.close(
+    # Query docs
+    # https://docs.sqlalchemy.org/en/13/orm/query.html
+    result_query = session.query(State)\
+        .filter(State.name == word_to_match).first()
+    print(result_query.id if result_query else "Not found")
+    # Close session
+    session.close()
